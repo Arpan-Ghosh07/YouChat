0734402548c05a87efa504550a971fa677d4f8c1
Arpan <arpanghosh1029@gmail.com>
2026-02-06 20:38:47 +0530

    first comment

---
+++ client/build/precache-manifest.9d5f3741241edc783edd373fc7bdbe32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc850c0644614bdd6690b85bc16e6bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e8a5dd10a608e403bfe",
+    "url": "/static/js/2.49284e7b.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.49284e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aead5eb7ecbfbed0df3",
+    "url": "/static/js/main.f7c127d7.chunk.js"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "88108281b94e79aba081635756f943d8",
+    "url": "/static/media/YouChat.88108281.png"
+  }
+]);
